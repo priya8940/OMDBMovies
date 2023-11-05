9de1f014ad6d5a1a7fa7f517f93c565d70458ad5
tarif.m <tarif.m@zohocorp.com>
2023-11-05 12:53:48 +0530

    Adding Id in each movie

--- js/homepage.js
+++ js/homepage.js
@@ -4,19 +4,30 @@
 
 const btnElement = document.getElementById('btn');
 const searchInputElement = document.getElementById('search-input');
+const container = document.getElementById('search-results');
 btnElement.addEventListener('click', async ()=>{
     const searchPattern = searchInputElement.value;
     const respons = await fetch(`https://www.omdbapi.com/?s=${searchPattern}&page=2&apikey=ee731cd5`);
     const data = await respons.json();
     const movieArray = data.Search;
-    const container = document.getElementById('search-results');
+    //remove old search from container
+    container.innerHTML = "";
     for(let movie of movieArray){
+        //creating image element and setting src attribute
         let imgTag = document.createElement('img');
         imgTag.setAttribute('src',movie.Poster);
+
+        //creating div element and setting class result-item
         let divEle = document.createElement('div');
         divEle.setAttribute('class','result-item');
+
+        //adding movie id
+        const movieId = movie.imdbID;
+        divEle.id = movieId;
+        //adding image element as child of div element
         divEle.appendChild(imgTag);
 
+        //putting newly created div element into container
         container.appendChild(divEle);
     }
     
