4ab79da43a0134830ee148d3e4eb3d4980e1d699
tarif.m <tarif.m@zohocorp.com>
2023-11-08 15:45:39 +0530

    Adding Movie details specific changes

--- js/homepage.js
+++ js/homepage.js
@@ -2,7 +2,8 @@ const btnElement = document.getElementById('btn');
 const searchInputElement = document.getElementById('search-input');
 const container = document.getElementById('search-results');
 const favoriteMovies = [];
-btnElement.addEventListener('click', async ()=>{
+btnElement.addEventListener('click', async (event)=>{
+    event.stopPropagation();
     const searchPattern = searchInputElement.value;
     const respons = await fetch(`https://www.omdbapi.com/?s=${searchPattern}&page=2&apikey=ee731cd5`);
     const data = await respons.json();
@@ -18,6 +19,8 @@ btnElement.addEventListener('click', async ()=>{
         let divEle = document.createElement('div');
         divEle.setAttribute('class','result-item');
 
+        //setting event listener for movie details
+        divEle.addEventListener('click',movieDetails);
         //adding movie id
         divEle.id = movie.imdbID;
 
@@ -45,6 +48,7 @@ btnElement.addEventListener('click', async ()=>{
 //Liking and disliking function
 
 function likeOrDislike(event){
+    event.stopPropagation();
     const parent = event.target.parentElement;
     const movieId = parent.id;
     if(event.target.classList.contains('favorite')){
@@ -56,7 +60,7 @@ function likeOrDislike(event){
         //search movieId in array and remove it
         let ind = -1;
         for(let i=0; i<favoriteMovies.length;i++){
-            if(favoriteMovies[i]===movieId){
+            if(favoriteMovies[i].id===movieId){
                 ind = i;
             }
         }
@@ -69,8 +73,92 @@ function likeOrDislike(event){
         event.target.classList.add('fa-solid');
         event.target.classList.remove('fa-regular');
        
+        //check if movie is already in the array
+        let ind = -1;
+        for(let i=0; i<favoriteMovies.length;i++){
+            if(favoriteMovies[i].id===movieId){
+                ind = i;
+            }
+        }
         //add movie id to fav array
-        favoriteMovies.push(movieId);
+        if(ind===-1){
+            favoriteMovies.push(parent);
+        }
     }
-    console.log(favoriteMovies);
+}
+
+
+//liked movies page
+const likeedButton = document.getElementById('liked-movies');
+
+likeedButton.addEventListener('click', (event)=>{
+    event.stopPropagation();
+    container.innerHTML = "";
+    for(let movie of favoriteMovies){
+        container.appendChild(movie);
+    }
+})
+
+async function movieDetails(event){
+    event.stopPropagation();
+    //console.log(event.target.parentElement.id);
+    const respons = await fetch(`http://www.omdbapi.com/?apikey=ee731cd5&i=${event.target.parentElement.id}`);
+    const data = await respons.json();
+    container.innerHTML = "";
+
+    const h3Ele = document.createElement('h3');
+    h3Ele.innerText = `TITLE : ${data.Title}`;
+
+    const pYearEle = document.createElement('p');
+    pYearEle.innerText = `YEAR : ${data.Year}`;
+
+    const pRatedEle = document.createElement('p');
+    pRatedEle.innerText = `RATED : ${data.Rated}`;
+
+    const pReleasedEle = document.createElement('p');
+    pReleasedEle.innerText = `RELEASED : ${data.Released}`;
+
+    const pGenreEle = document.createElement('p');
+    pGenreEle.innerText = `GENERE : ${data.Genre}`;
+
+    const pActorsEle = document.createElement('p');
+    pActorsEle.innerText = `ACTOR : ${data.Actors}`;
+
+    const pPlotEle = document.createElement('p');
+    pPlotEle.innerText = `PLOT : ${data.Plot}`;
+
+    const pLanguageEle = document.createElement('p');
+    pLanguageEle.innerText = `LANGUAGE : ${data.Language}`;
+
+    const pCountryEle = document.createElement('p');
+    pCountryEle.innerText = `COUNTRY : ${data.Country}`;
+
+    const pTypeEle = document.createElement('p');
+    pTypeEle.innerText = `TYPE : ${data.Type}`;
+
+    const divEle = document.createElement('div');
+    divEle.classList.add('details');
+
+    divEle.appendChild(h3Ele);
+    divEle.appendChild(pYearEle);
+    divEle.appendChild(pRatedEle);
+    divEle.appendChild(pReleasedEle);
+    divEle.appendChild(pGenreEle);
+    divEle.appendChild(pActorsEle);
+    divEle.appendChild(pPlotEle);
+    divEle.appendChild(pLanguageEle);
+    divEle.appendChild(pCountryEle);
+    divEle.appendChild(pTypeEle);
+
+    const imgEle = document.createElement('img');
+    imgEle.setAttribute('src',data.Poster);
+
+    const divEle2 = document.createElement('div');
+    divEle2.classList.add('movie-details');
+
+    divEle2.appendChild(imgEle);
+    divEle2.appendChild(divEle);
+
+    container.appendChild(divEle2);
+
 }
